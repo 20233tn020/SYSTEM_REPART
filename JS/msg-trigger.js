d649220fa7854c9b47ea8a0fb5a60f031439619a
20233tn020 <20233tn020@utez.edu.mx>
2025-04-30 00:26:53 -0600

    Update msg-trigger.js

--- JS/msg-trigger.js
+++ JS/msg-trigger.js
@@ -1,17 +1,5 @@
-// Número codificado en base64 y luego invertido manualmente
-const codificado = "==Q8O2z3Y6TM7c7z25zK".split('').reverse().join('');
-
-// Función para decodificar Base64
-function decodificar(base64) {
-  try {
-    return atob(base64);
-  } catch (e) {
-    return '';
-  }
-}
-
 document.addEventListener("DOMContentLoaded", () => {
-  const numero = decodificar(codificado); // Resultado:
+  const numero = atob("KzUyNzc3MTYzMjA4OQ==").replace(/\D/g, ""); // Elimina el "+"
   const mensaje = encodeURIComponent("¡Hola! Estoy interesado en sus servicios.");
   const link = `https://wa.me/${numero}?text=${mensaje}`;
 
